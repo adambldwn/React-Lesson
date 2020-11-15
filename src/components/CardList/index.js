780cddc2054d91e73c0792eeae32c74561861106
efe <adambldwn110@gmail.com>
2020-11-15 23:17:56 +0300

    ders

---
+++ src/components/CardList/index.js
@@ -0,0 +1,20 @@
+import {useContext} from 'react';
+import {Card} from "../Card";
+import {MovieContext} from "../../App";
+import {StyledCardList} from "./CardList.style"
+
+export const CardList = () => {
+  const {movieList, baseImageUrl} = useContext(MovieContext);
+    return (
+        <StyledCardList>
+           {movieList?.map(
+               (movie, index) => (
+               <Card 
+                key={index} 
+                title={movie.title}
+                imgSrc={baseImageUrl + movie.poster_path}
+                />)
+            )}
+        </StyledCardList>
+    )
+}
